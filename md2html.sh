7d84cc7c9aa38859433e91515785e2b412771f08
SriMethan <77402282+SriMethan@users.noreply.github.com>
2022-07-28 22:27:08 +0530

    Revert Changes

--- md2html.sh
+++ md2html.sh
@@ -17,9 +17,15 @@ for f in *.md; do
 done
 
 
+<<<<<<< HEAD
 SRC='https://github.com/gbtami/pychess-variants/blob/master'; 
 #DST='https://www.pychess.org';
 DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@1.8.4';
+=======
+SRC='https://github.com/SriMethan/liantichess/blob/master'; 
+#DST='https://liantichess.herokuapp.com';
+DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@1.7.7';
+>>>>>>> parent of 5babe3c5 (Resolve Merge Conflicts)
 find . -type f -name "*.html" -exec perl -pi -e s,$SRC,$DST,g '{}' +
 
 mkdir -p ../../templates/docs
